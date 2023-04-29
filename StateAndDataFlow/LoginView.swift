6aa82a1a1e90856a72709bb2f1784cdf068ec048
Anton Kuzmin <furthertwoant@gmail.com>
2023-04-29 15:41:28 +0400

    Initial Commit

---
+++ StateAndDataFlow/LoginView.swift
@@ -0,0 +1,57 @@
+//
+//  LoginView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 26.04.2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var name = ""
+    @State private var characterCount = 0
+    @EnvironmentObject private var userSettings: UserSettings
+    
+    var body: some View {
+        VStack {
+            TextField("Enter your name", text: $name)
+                .multilineTextAlignment(.center)
+                .onChange(of: name) { characterCount = $0.count }
+            Button(action: login) {
+                HStack {
+                    Image(systemName: "checkmark.circle")
+                    Text("Ok")
+                }
+            }
+            .disabled(characterCount < 3)
+            CharactersCountView(
+                count: $characterCount,
+                color: characterCount < 3 ? .red : .green
+            )
+        }
+    }
+    
+    private func login() {
+        if !name.isEmpty {
+            userSettings.name = name
+            userSettings.isLoggedIn.toggle()
+        }
+    }
+}
+
+struct CharactersCountView: View {
+    @Binding var count: Int
+    
+    let color: Color
+    
+    var body: some View {
+        Text(count.formatted())
+            .foregroundColor(color)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ StateAndDataFlow/UserSettings.swift
@@ -0,0 +1,14 @@
+//
+//  UserSettings.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 26.04.2023.
+//
+
+import Foundation
+import SwiftUI
+
+final class UserSettings: ObservableObject {
+    @AppStorage("isLoggedIn") var isLoggedIn = false
+    @AppStorage("name") var name: String = ""
+}
